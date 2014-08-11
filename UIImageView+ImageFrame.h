e1dac3c8ebb2ee11d155da1d6b80c41e222e8e95
Aleksey Garbarev <alex.garbarev@gmail.com>
2014-08-11 17:42:41 +0700

    Initial (and maybe final) commmit

---
+++ UIImageView+ImageFrame.h
@@ -0,0 +1,19 @@
+//
+//  UIImageView+ImageFrame.h
+//
+//  Created by Aleksey Garbarev on 01.10.13.
+//  Copyright (c) 2013 Aleksey Garbarev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (ImageFrame)
+
+- (CGRect) imageFrame;
+
+- (CGPoint) imageScales;
+
++ (CGRect) imageFrameForImageSize:(CGSize)imageSize boundingRect:(CGRect)boundingRect contentMode:(UIViewContentMode)contentMode;
++ (CGPoint) imageScalesForImageSize:(CGSize)imageSize boundingRect:(CGRect)boundingRect contentMode:(UIViewContentMode)contentMode;
+
+@end
